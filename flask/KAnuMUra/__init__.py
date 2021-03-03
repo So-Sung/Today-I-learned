24e01fc49c8ac6f940e09074f7fed3c17b2b33dd
jihwan_kim <5nlylaw@naver.com>
2021-03-03 16:09:01 +0900

    Flask  공부

---
+++ flask/KAnuMUra/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask 
+
+def create_app(): # 애플리케이션 팩토리 
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello_flask():
+        return "Welcome to Flask!!"
+
+    return app
+# -----------------------
