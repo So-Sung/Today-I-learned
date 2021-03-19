e7eca5d386e8dccf01e55d1f6159da004cbe81f7
Jihwan_Kim <77475809+So-Sung@users.noreply.github.com>
2021-03-19 14:28:36 +0900

    Update __init__.py

--- flask/KAnuMUra/__init__.py
+++ flask/KAnuMUra/__init__.py
@@ -1,11 +1,24 @@
+import os
 from flask import Flask 
+from flask import render_template
 
-def create_app(): # 애플리케이션 팩토리 
+
+def create_app(): 
     app = Flask(__name__)
+
+
 
     @app.route('/')
     def hello_flask():
-        return "Welcome to Flask!!!"
-
+        return render_template("Daily.html")
+    @app.route('/a/')
+    def Board():
+        return render_template("index.html")
+    @app.route('/b/')
+    def About():
+        return render_template("About.html")
+    @app.route('/c/')
+    def eAbout():
+        return render_template("test.html")    
     return app
 # -----------------------
